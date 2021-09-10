6f62803d069b08c3e8eeafa0d5ffcd5ecc8affc0
AnuragMurti28 <anuragmurti28@gmail.com>
2021-09-11 02:32:53 +0530

    var and let tested

---
+++ tute4.js
@@ -0,0 +1,12 @@
+// Primitive datatypes
+
+// String
+var name = "Anurag";
+console.log("This tutorial is practicing by " + name);
+console.log("Data type is " + typeof(name));
+document.getElementsByClassName("string").innerHTML("Data type is " + typeof(name));
+
+// Numbers
+let marks = 34;
+console.log("Data type is " + typeof(marks));
+document.getElementsByClassName("number").innerHTML("Data type is " + typeof(marks));
