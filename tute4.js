0380992095bc3f2727a617a075b4df6508cd8c61
AnuragMurti28 <anuragmurti28@gmail.com>
2021-09-24 22:43:28 +0530

    tute4 completed

--- tute4.js
+++ tute4.js
@@ -1,12 +1,43 @@
 // Primitive datatypes
 
 // String
-var name = "Anurag";
-console.log("This tutorial is practicing by " + name);
-console.log("Data type is " + typeof(name));
-document.getElementsByClassName("string").innerHTML("Data type is " + typeof(name));
+var myname = "Anurag";
+console.log("This tutorial is practicing by " + myname);
+console.log("Data type of name is " + typeof(myname));
+
 
 // Numbers
 let marks = 34;
-console.log("Data type is " + typeof(marks));
-document.getElementsByClassName("number").innerHTML("Data type is " + typeof(marks));
+console.log("Data type of marks is " + typeof(marks));
+
+
+// boolean
+let type = 3 > 5;
+console.log("Data type of type is " + typeof(type));
+
+// Undefined
+let undef = undefined;
+console.log("Data type of undef is " + typeof(undef));
+
+// Arrays
+myarr = [1, 2, 3, 4, false, "string"];
+console.log("Data type of undef is " + typeof(myarr));
+
+// object literals
+let stMarks = {
+    anurag: 87,
+    harshita: 67,
+    shubham: 51,
+};
+
+console.log(stMarks);
+console.log("data type of stMarks is " + typeof(stMarks));
+
+// functions
+function findName() {
+
+}
+console.log(typeof findName);
+
+let date = new Date();
+console.log(typeof date);
