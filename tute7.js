8bd14b429c622a58894869a99143bb0b3da7d91b
AnuragMurti28 <anuragmurti28@gmail.com>
2021-09-28 02:05:24 +0530

    tute7.js updated

--- tute7.js
+++ tute7.js
@@ -4,7 +4,9 @@ const fruits = ['Orange', 'Apple', 'Pineapple', 'Banana'];
 const mixed = ['str', 89, [35, 68]];
 const arr = new Array(23, 46, 'Peach', 87);
 console.log('arr: ', arr);
-console.log('mixed', mixed);
+console.log('mixed: ', mixed);
+console.log('fruits: ', fruits);
+console.log('marks: ', marks);
 console.log('fruits[1]: ', fruits[1]);
 
 console.log("modify array using index assigning arr[1]= 334");
@@ -13,3 +15,18 @@ console.log("arr after changing arr[1] element: ", arr);
 console.log("assigning value of array to a variable");
 let marks3 = marks[3];
 console.log("assigned value of marks[3] to marks3: ", marks3);
+
+console.log("arr.length is property: not method", arr.length);
+console.log("isArray() is method returns boolean if passed value(Pineapple) is an array or not: ", Array.isArray('Pineapple'));
+console.log("isArray() is method returns boolean if passed value(marks) is an array or not: ", Array.isArray(marks));
+console.log("isArray() is method returns boolean if passed value(arr) is an array or not: ", Array.isArray(arr));
+
+let value = marks.indexOf(77);
+console.log("indexof(77) returns the index of the value passed: ", value);
+console.log("value = marks.indexof(77): ", value);
+console.log("unshift() inserts element in starting of array");
+marks.unshift(1009);
+console.log("marks[] after unshift(1009): ", marks);
+console.log("push() insert element in last of array");
+fruits.push('Papaya');
+console.log("fruits[] after push(Papaya): ", fruits);
