1723b4f02a879f90bdf91f07019336d25db4675d
AnuragMurti28 <anuragmurti28@gmail.com>
2021-09-28 01:21:49 +0530

    tute6.js completed

--- tute6.js
+++ tute6.js
@@ -1,12 +1,12 @@
 console.log("we are at tute6");
-const name = 'Anurag';
+const author = 'Anurag';
 const greeting = 'Good Morning';
-console.log(greeting + " " + name);
+console.log(greeting + " " + author);
 
 let html;
 html = '<h1> this is heading</h1>' + '<p>this is my para</p>';
 
-html = html.concat('this');
+html = html.concat('this', 'str2');
 console.log(html);
 console.log("length of HTML string: ", html.length);
 console.log("toLowerCase() of HTML string: ", html.toLowerCase());
@@ -16,6 +16,29 @@ console.log("starts from 0 and end noo limit: " +
     html[7]);
 
 console.log(html.indexOf('g'));
-console.log("using lastIndexOf", html.lastIndexOf(6));
-console.log("using for charAt", html.charAt(6));
-console.log(html.endsWith(false));
+console.log("using lastIndexOf", html.lastIndexOf('h1'));
+console.log("using IndexOf", html.indexOf('h1'));
+console.log("using IndexOf for unavailable substring", html.indexOf('strong'));
+console.log("using for charAt: ", html.charAt(6));
+console.log("using endsWith() function returns boolean if string ends with the passed value(false) in function: ", html.endsWith(false));
+console.log("using endsWith() function returns boolean if string ends with the passed value(str2) in function: ", html.endsWith('str2'));
+console.log("using includes() function returns boolean if string have substring as passed value(str2) in function: ", html.includes('str2'));
+console.log("using includes() function returns boolean if string have substring as passed value(false) in function: ", html.includes(false));
+console.log("using substring() function returns the elements at index passed in function if string have substring as passed value(1,9) in function: ", html.substring(1, 9));
+let htmlarr = html.split(' ');
+console.log("using split() function splits the string from the value that has been passed in split()(htmlarr = html.split(' ')) function as an array: ", htmlarr);
+let htmlarr2 = html.split('<');
+console.log("using split() function splits the string from the value that has been passed in split()(htmlarr2 = html.split('<')) function as an array: ", htmlarr2);
+console.log("using slice() function returns the elements from rrange index passed in function if string have substring as passed value(1,9) in function: ", html.slice(1, 9));
+
+console.log('compare: ', html);
+console.log("using replace(1st,2nd) function replaces the frist occurence of 1st value (1st value from 2nd value): ", html.replace('is', 'that'));
+
+console.log("Template Literals");
+let fruit1 = 'Orange';
+let fruit2 = 'Apple';
+let myhtml = `Hello ${author}
+    <h1> This is Example of Template literals</h1>
+    <p>You like ${fruit1} and ${fruit2} </p>
+`;
+document.body.innerHTML = myhtml;
