b9e4b271f22e538c9e44bf897232709021dbcf07
AnuragMurti28 <anuragmurti28@gmail.com>
2021-09-28 01:34:23 +0530

    tute7.js created

---
+++ tute7.js
@@ -0,0 +1,15 @@
+console.log("we are in tute7.js and let discuss arrays and ojects");
+const marks = [81, 54, 68, 78, 77, 88];
+const fruits = ['Orange', 'Apple', 'Pineapple', 'Banana'];
+const mixed = ['str', 89, [35, 68]];
+const arr = new Array(23, 46, 'Peach', 87);
+console.log('arr: ', arr);
+console.log('mixed', mixed);
+console.log('fruits[1]: ', fruits[1]);
+
+console.log("modify array using index assigning arr[1]= 334");
+arr[1] = 334;
+console.log("arr after changing arr[1] element: ", arr);
+console.log("assigning value of array to a variable");
+let var1 = marks[3];
+console.log("assigned value of marks[3] to var1: ", var1);
