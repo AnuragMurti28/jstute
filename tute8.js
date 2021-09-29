51594750b695136e9c601c221ff7f9595dd5476a
AnuragMurti28 <anuragmurti28@gmail.com>
2021-09-30 02:17:47 +0530

    tute8.js creaated

---
+++ tute8.js
@@ -0,0 +1,8 @@
+console.log("This is tute8.js");
+const age = 19;
+console.log("If and else condition for adult and senior citizen");
+if (age >= 19) {
+    console.log("Adult");
+} else if (age >= 60) {
+    console.log("Senior Citizen");
+}
