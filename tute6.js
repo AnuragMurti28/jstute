e3092b0ab265d8215394aef3377ad9ee20032717
AnuragMurti28 <anuragmurti28@gmail.com>
2021-09-30 01:58:19 +0530

    tute6.js updated

--- tute6.js
+++ tute6.js
@@ -24,12 +24,12 @@ console.log("using endsWith() function returns boolean if string ends with the p
 console.log("using endsWith() function returns boolean if string ends with the passed value(str2) in function: ", html.endsWith('str2'));
 console.log("using includes() function returns boolean if string have substring as passed value(str2) in function: ", html.includes('str2'));
 console.log("using includes() function returns boolean if string have substring as passed value(false) in function: ", html.includes(false));
-console.log("using substring() function returns the elements at index passed in function if string have substring as passed value(1,9) in function: ", html.substring(1, 9));
+console.log("using substring() function returns the elements at index passed in function if string have substring as passed value(1,11) in function: ", html.substring(0, 11));
 let htmlarr = html.split(' ');
 console.log("using split() function splits the string from the value that has been passed in split()(htmlarr = html.split(' ')) function as an array: ", htmlarr);
 let htmlarr2 = html.split('<');
 console.log("using split() function splits the string from the value that has been passed in split()(htmlarr2 = html.split('<')) function as an array: ", htmlarr2);
-console.log("using slice() function returns the elements from rrange index passed in function if string have substring as passed value(1,9) in function: ", html.slice(1, 9));
+console.log("using slice() function returns the elements from range index passed in function if string have substring as passed value(1,9) in function: ", html.slice(1, 9));
 
 console.log('compare: ', html);
 console.log("using replace(1st,2nd) function replaces the frist occurence of 1st value (1st value from 2nd value): ", html.replace('is', 'that'));
