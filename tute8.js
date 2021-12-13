4fc291bf4e3de7d3842d8d956f7a3a1fd814c632
AnuragMurti28 <anuragmurti28@gmail.com>
2021-12-13 17:53:11 +0530

    tute9.js added

--- tute8.js
+++ tute8.js
@@ -1,8 +1,22 @@
-console.log("This is tute8.js");
-const age = 19;
+console.log("This is tutorial 8.js");
+const age = 24;
+// const vari = 34;
+const doesDrive = true;
 console.log("If and else condition for adult and senior citizen");
 if (age >= 19) {
     console.log("Adult");
 } else if (age >= 60) {
     console.log("Senior Citizen");
+}
+
+if (typeof vari !== 'undefined') {
+    console.log("vari is defined");
+} else {
+    console.log('vari is not defined');
+}
+
+if (doesDrive) {
+    console.log("You can drive");
+} else {
+    console.log("You cannot drive");
 }
---
+++ tute9.js
@@ -0,0 +1,7 @@
+console.log("this is tutorial 9");
+// for() while() do-while() loop
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+
+}
