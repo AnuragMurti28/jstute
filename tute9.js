207873eef4001cb748befdc5687c32545fae7212
AnuragMurti28 <anuragmurti28@gmail.com>
2021-12-20 16:09:29 +0530

    tute9.js completed

--- tute9.js
+++ tute9.js
@@ -2,6 +2,11 @@ console.log("this is tutorial 9");
 // for() while() do-while() loop
 
 for (let i = 0; i < 10; i++) {
-    console.log(i);
+    console.log(i + 1);
 
+}
+
+let j = 0;
+while (j < 25) {
+    console.log(j + 1);
 }
